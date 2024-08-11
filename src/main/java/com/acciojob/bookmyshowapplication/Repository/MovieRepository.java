aadc40e6b51aa6a52cfea2cd9f220a163afff20a
Aatish <aatishkumarsingh30@gmail.com>
2024-08-11 23:52:16 +0530

    done

---
+++ src/main/java/com/acciojob/bookmyshowapplication/Repository/MovieRepository.java
@@ -0,0 +1,24 @@
+package com.acciojob.bookmyshowapplication.Repository;
+
+import com.acciojob.bookmyshowapplication.Entities.Movie;
+import com.acciojob.bookmyshowapplication.Enums.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface MovieRepository extends JpaRepository<Movie,Integer> {
+
+    //2nd type of fetch where you just define the method
+    //no query nothing
+    //but naming of the method has to be strict
+
+    Movie findMovieByMovieNameAndMovieLanguage(String movieName, Language language);
+
+    Movie findMovieByMovieName(String movieName);
+
+    List<Movie> findAllByDurationGreaterThanEqual(double duration);
+
+}
